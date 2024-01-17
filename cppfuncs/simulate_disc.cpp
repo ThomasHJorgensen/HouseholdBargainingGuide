2c9629050e95ef92ff063d222f6c96b158f4d32d
ThomasHJorgensen <tjo@econ.ku.dk>
2024-01-17 16:55:06 +0100

    continuous power in simulation

--- cppfuncs/simulate_disc.cpp
+++ cppfuncs/simulate_disc.cpp
@@ -7,11 +7,11 @@
 
 namespace sim {
 
-    int update_power(int t, double power_lag, double love,double A_lag,double Aw_lag,double Am_lag,sim_struct* sim, sol_struct* sol, par_struct* par){
+    int update_power_index(int t, int power_idx_lag, double love,double A_lag,double Aw_lag,double Am_lag,sim_struct* sim, sol_struct* sol, par_struct* par){
         
         // setup
-        double power = -1.0; // initialize as divorce
-        int idx_sol = index::index4(t,0,0,0,par->T,par->num_power,par->num_love,par->num_A); 
+        int power_idx = -1; // initialize as divorce
+        int idx_sol = index::index4(t,power_idx_lag,0,0,par->T,par->num_power,par->num_love,par->num_A); 
         double Vw_couple, Vm_couple;
 
         // value of transitioning into singlehood
@@ -19,65 +19,55 @@ namespace sim {
         double Vw_single = tools::interp_1d(par->grid_Aw,par->num_A,&sol->Vw_trans_single[idx_single],Aw_lag);
         double Vm_single = tools::interp_1d(par->grid_Am,par->num_A,&sol->Vm_trans_single[idx_single],Am_lag);
 
-        tools::interp_3d_2out(par->grid_power,par->grid_love,par->grid_A, par->num_power,par->num_love,par->num_A, &sol->Vw_remain_couple[idx_sol],&sol->Vm_remain_couple[idx_sol], power_lag,love,A_lag, &Vw_couple, &Vm_couple);
+        tools::interp_2d_2out(par->grid_love,par->grid_A,par->num_love,par->num_A,&sol->Vw_remain_couple[idx_sol],&sol->Vm_remain_couple[idx_sol],love,A_lag, &Vw_couple, &Vm_couple);
         
         // check participation constraints
         if ((Vw_couple>=Vw_single) & (Vm_couple>=Vm_single)){
-            power = power_lag;
+            power_idx = power_idx_lag;
 
         } else {
 
             if ((Vm_couple>=Vm_single)){ // woman want to leave
 
-                // find relevant values of remaining a couple at all other states than power
-                double* Vw_power = new double[par->num_power];
-                for (int iP=0; iP<par->num_power; iP++){ 
+                for (int iP=power_idx_lag+1; iP<par->num_power; iP++){ // increase power of women
                     int idx = index::index4(t,iP,0,0,par->T,par->num_power,par->num_love,par->num_A); 
-                    Vw_power[iP] = tools::interp_2d(par->grid_love,par->grid_A,par->num_love,par->num_A,&sol->Vw_remain_couple[idx],love,A_lag);
+                    tools::interp_2d_2out(par->grid_love,par->grid_A,par->num_love,par->num_A,&sol->Vw_remain_couple[idx],&sol->Vm_remain_couple[idx],love,A_lag, &Vw_couple, &Vm_couple);
+                    
+                    // check participation constraint
+                    double Sw = couple::calc_marital_surplus(Vw_couple,Vw_single,par);
+                    double Sm = couple::calc_marital_surplus(Vm_couple,Vm_single,par);
+
+                    // update index if a solution is found
+                    if((Sw>=0.0)&(Sm>=0.0)){
+                        power_idx = iP; 
+                        break;
+                    }
+                    
                 }
-
-                // interpolate the power based on the value of single to find indifference-point. (flip the axis so to speak)
-                power = tools::interp_1d(Vw_power, par->num_power, par->grid_power, Vw_single);
-                delete Vw_power;
-
-                // calculate value for man at this level of power
-                double Vm_power = tools::interp_3d(par->grid_power,par->grid_love,par->grid_A, par->num_power,par->num_love,par->num_A, &sol->Vm_remain_couple[idx_sol], power,love,A_lag);
-                double Sm = couple::calc_marital_surplus(Vm_power,Vm_single,par);
-                
-                // check if he is happy
-                if(Sm<0.0){
-                    power = -1.0;  // he is not -> divorce
-                }
-
-                
 
             } else { // man want to leave
 
-                // find relevant values of remaining a couple at all other states than power
-                double* Vm_power = new double[par->num_power];
-                for (int iP=0; iP<par->num_power; iP++){ 
+                for (int iP=power_idx_lag-1; iP>=0; iP--){ // increase power of men
                     int idx = index::index4(t,iP,0,0,par->T,par->num_power,par->num_love,par->num_A); 
-                    Vm_power[iP] = tools::interp_2d(par->grid_love,par->grid_A,par->num_love,par->num_A,&sol->Vm_remain_couple[idx],love,A_lag);
-                }
+                    tools::interp_2d_2out(par->grid_love,par->grid_A,par->num_love,par->num_A,&sol->Vw_remain_couple[idx],&sol->Vm_remain_couple[idx],love,A_lag, &Vw_couple, &Vm_couple);
 
-                // interpolate the power based on the value of single to find indifference-point. (flip the axis so to speak)
-                power = tools::interp_1d(Vm_power, par->num_power, par->grid_power, Vm_single);
-                delete Vm_power;
+                    // check participation constraint
+                    double Sw = couple::calc_marital_surplus(Vw_couple,Vw_single,par);
+                    double Sm = couple::calc_marital_surplus(Vm_couple,Vm_single,par);
 
-                // calculate value for woman at this level of power
-                double Vw_power = tools::interp_3d(par->grid_power,par->grid_love,par->grid_A, par->num_power,par->num_love,par->num_A, &sol->Vw_remain_couple[idx_sol], power,love,A_lag);
-                double Sw = couple::calc_marital_surplus(Vw_power,Vw_single,par);
-                
-                // check if she is happy
-                if(Sw<0.0){
-                    power = -1.0;  // she is not -> divorce
+                    // update index if a solution is found
+                    if((Sw>=0.0)&(Sm>=0.0)){
+                        power_idx = iP; 
+                        break;
+                    }
+                    
                 }
 
             }
         }
 
-        return power;
-    } // update_power
+        return power_idx;
+    } // update_power_index
 
 
     void model(sim_struct *sim, sol_struct *sol, par_struct *par){
@@ -97,7 +87,7 @@ namespace sim {
                     double Aw_lag = sim->init_Aw[i];
                     double Am_lag = sim->init_Am[i];
                     bool couple_lag = sim->init_couple[i];
-                    double power_lag = par->grid_power[sim->init_power_idx[i]];
+                    int power_idx_lag = sim->init_power_idx[i];
                     double love = sim->init_love[i];
 
                     if (t>0){
@@ -105,7 +95,7 @@ namespace sim {
                         Aw_lag = sim->Aw[it_1];
                         Am_lag = sim->Am[it_1];
                         couple_lag = sim->couple[it_1];
-                        power_lag = sim->power[it_1];
+                        power_idx_lag = sim->power_idx[it_1];
                         love = sim->love[it];
                     } else {
                         sim->love[it] = love;
@@ -113,12 +103,12 @@ namespace sim {
 
                     
                     // first check if they want to remain together and what the bargaining power will be if they do.
-                    int power;
+                    int power_idx;
                     if (couple_lag) {
 
-                        power = update_power(t,power_lag,love,A_lag,Aw_lag,Am_lag,sim,sol,par);
+                        power_idx = update_power_index(t,power_idx_lag,love,A_lag,Aw_lag,Am_lag,sim,sol,par);
 
-                        if (power < 0.0) { // divorce is coded as -1
+                        if (power_idx < 0) { // divorce is coded as -1
                             sim->couple[it] = false;
 
                         } else {
@@ -130,14 +120,14 @@ namespace sim {
                     }
 
                     // update behavior
+                    int idx_sol = index::index4(t,power_idx,0,0,par->T,par->num_power,par->num_love,par->num_A); 
                     if (sim->couple[it]){
                         
-                        // optimal consumption allocation if couple (interpolate in power, love, A)
-                        int idx_sol = index::index4(t,0,0,0,par->T,par->num_power,par->num_love,par->num_A); 
-                        double C_tot = tools::interp_3d(par->grid_power,par->grid_love,par->grid_A,par->num_power,par->num_love,par->num_A ,&sol->C_tot_couple[idx_sol],power,love,A_lag);
+                        // optimal consumption allocation if couple
+                        double C_tot = tools::interp_2d(par->grid_love,par->grid_A,par->num_love,par->num_A ,&sol->C_tot_couple[idx_sol],love,A_lag);
 
                         double C_pub = 0.0;
-                        couple::intraperiod_allocation_sim(&sim->Cw_priv[it], &sim->Cm_priv[it], &C_pub,  C_tot,power,sol,par); 
+                        couple::intraperiod_allocation(&sim->Cw_priv[it], &sim->Cm_priv[it], &C_pub,  C_tot,power_idx,sol,par); 
                         sim->Cw_pub[it] = C_pub;
                         sim->Cm_pub[it] = C_pub;
 
@@ -152,10 +142,8 @@ namespace sim {
                         sim->Aw[it] = par->div_A_share * sim->A[it];
                         sim->Am[it] = (1.0-par->div_A_share) * sim->A[it];
 
-                        sim->power[it] = power;
-
-                        // sim->power_idx[it] = power_idx;
-                        // sim->power[it] = par->grid_power[power_idx];
+                        sim->power_idx[it] = power_idx;
+                        sim->power[it] = par->grid_power[power_idx];
 
                     } else { // single
 
@@ -163,7 +151,7 @@ namespace sim {
                         int idx_sol_single = index::index2(t,0,par->T,par->num_A);
                         double *sol_single_w = &sol->Cw_tot_trans_single[idx_sol_single];
                         double *sol_single_m = &sol->Cm_tot_trans_single[idx_sol_single];
-                        if (power_lag<0){
+                        if (power_idx_lag<0){
                             sol_single_w = &sol->Cw_tot_single[idx_sol_single];
                             sol_single_m = &sol->Cm_tot_single[idx_sol_single];
                         } 
@@ -181,8 +169,7 @@ namespace sim {
                         sim->Aw[it] = Mw - sim->Cw_priv[it] - sim->Cw_pub[it];
                         sim->Am[it] = Mm - sim->Cm_priv[it] - sim->Cm_pub[it];
 
-                        // sim->power_idx[it] = -1;
-                        sim->power[it] = -1.0;
+                        sim->power_idx[it] = -1;
 
                         // left as nans by not updating them:
                         // sim->power[it1] = nan
